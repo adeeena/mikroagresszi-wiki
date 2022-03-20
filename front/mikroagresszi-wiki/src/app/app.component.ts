12108112673d2aed4b1185a9c4008a7957ab466f
Muráncsik Adéna <adena.murancsik@gmail.com>
2022-03-20 22:41:14 +0100

    Sidebar works

--- front/mikroagresszi-wiki/src/app/app.component.ts
+++ front/mikroagresszi-wiki/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mikroagresszi-wiki';
+  public isOpened: boolean = false;
+
+  toggleSidenav() {
+    this.isOpened = !this.isOpened;
+  }
+
+
 }
--- front/mikroagresszi-wiki/src/app/app.module.ts
+++ front/mikroagresszi-wiki/src/app/app.module.ts
@@ -2,24 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule} from "@angular/material/toolbar";
 import { MatIconModule} from "@angular/material/icon";
+import { MatButtonModule} from "@angular/material/button";
 import { AppRoutingModule } from './app-routing.module';
+import {MatSidenavModule} from "@angular/material/sidenav";
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
