5116048d57b145e4702cdc7865c20122f6b95591
Muráncsik Adéna <sebestyenm@silversea.com>
2022-03-23 23:20:32 +0100

    Adding hash route + google tag

--- front/mikroagresszi-wiki/src/app/components/entry/entry.component.ts
+++ front/mikroagresszi-wiki/src/app/components/entry/entry.component.ts
@@ -50,7 +50,7 @@ export class EntryComponent implements OnInit {
     this.dialog.open(DialogElementsExampleDialog, {
       width: this.innerWidth <= 600 ? '90vw' : '50vw',
       data: {
-        link: location.origin + '/entry/' + this.entryId
+        link: location.origin + '/#/entry/' + this.entryId
       }
     });
   }
