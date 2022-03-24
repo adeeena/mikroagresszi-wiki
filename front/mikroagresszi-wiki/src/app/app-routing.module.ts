f0ab798c676808d217b3259d819c94da3ad75500
Muráncsik Adéna <sebestyenm@silversea.com>
2022-03-24 17:40:49 +0100

    Fixing few css issues.

--- front/mikroagresszi-wiki/src/app/app-routing.module.ts
+++ front/mikroagresszi-wiki/src/app/app-routing.module.ts
@@ -13,7 +13,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
