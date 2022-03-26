5405d44a2c8cf38596aa35095a48e6af28dfc542
Muráncsik Adéna <sebestyenm@silversea.com>
2022-03-26 23:00:51 +0100

    Fixing bad endpoint

--- front/mikroagresszi-wiki/src/app/services/entry.service.ts
+++ front/mikroagresszi-wiki/src/app/services/entry.service.ts
@@ -8,7 +8,7 @@ import {Entry} from "../models/entry";
 export class EntryService {
   private entryUrl:string = 'https://api.mikroagresszi.adena.dev/entry/getBy?categoryId=';
   private entryByUrl:string = 'https://api.mikroagresszi.adena.dev/entry/getById?entryId=';
-  private searchByUrl:string = 'http://api.mikroagresszi.adena.dev/entry/searchBy?query='
+  private searchByUrl:string = 'https://api.mikroagresszi.adena.dev/entry/searchBy?query=';
 
   /*private entryUrl:string = 'http://localhost:5071/entry/getBy?categoryId=';
   private entryByUrl:string = 'http://localhost:5071/entry/getById?entryId=';
