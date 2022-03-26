007117a673adf83a65210eab91ca4d26e4179f5a
Muráncsik Adéna <sebestyenm@silversea.com>
2022-03-26 15:30:06 +0100

    Animation render fix on ios

--- front/mikroagresszi-wiki/src/app/components/home/home.component.ts
+++ front/mikroagresszi-wiki/src/app/components/home/home.component.ts
@@ -9,7 +9,7 @@ import {fadeInUpOnEnterAnimation} from "angular-animations";
     fadeInUpOnEnterAnimation({ anchor: 'enter1', duration: 1000, delay: 100, translate: '30px' }),
     fadeInUpOnEnterAnimation({ anchor: 'enter2', duration: 1000, delay: 300, translate: '30px' }),
     fadeInUpOnEnterAnimation({ anchor: 'enter3', duration: 1000, delay: 500, translate: '30px' }),
-    fadeInUpOnEnterAnimation({ anchor: 'enter4', duration: 1000, delay: 700, translate: '30px' }),
+    fadeInUpOnEnterAnimation({ anchor: 'enter4', duration: 1000, delay: 900, translate: '30px' }),
   ]
 })
 export class HomeComponent {
