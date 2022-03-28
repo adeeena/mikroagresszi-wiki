1d25aaaa64c86beaf2a2509abef37a346dd6657c
Muráncsik Adéna <sebestyenm@silversea.com>
2022-03-28 13:13:18 +0200

    Adding missing dto profile

---
+++ api/MikroagressziWiki.Api/MikroagressziWiki.Api/DTOs/Profiles/CategoryEntriesResultDtoProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MikroagressziWiki.Logic.Models;
+
+namespace MikroagressziWiki.Api.DTOs.Profiles
+{
+    public class CategoryEntriesResultDtoProfile : Profile
+    {
+        public CategoryEntriesResultDtoProfile()
+        {
+            CreateMap<CategoryEntriesResultModel, CategoryEntriesResultDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.Entries, opt => opt.MapFrom(src => src.Entries));
+        }
+    }
+}
