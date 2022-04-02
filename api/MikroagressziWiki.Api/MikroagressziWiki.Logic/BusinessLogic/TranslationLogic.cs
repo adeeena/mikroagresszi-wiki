3fb93ef65bb43723c6f7f5e50901e7e81a739cc6
Muráncsik Adéna <sebestyenm@silversea.com>
2022-04-02 15:43:41 +0200

    Resetting cache for translation table.

--- api/MikroagressziWiki.Api/MikroagressziWiki.Logic/BusinessLogic/TranslationLogic.cs
+++ api/MikroagressziWiki.Api/MikroagressziWiki.Logic/BusinessLogic/TranslationLogic.cs
@@ -38,7 +38,7 @@ namespace MikroagressziWiki.Logic.BusinessLogic
         {
             string CACHE_KEY = "TRANSLATION_" + languageCode;
 
-            if (true || !_memoryCache.TryGetValue(CACHE_KEY, out object cacheValue))
+            if (!_memoryCache.TryGetValue(CACHE_KEY, out object cacheValue))
             {
                 MemoryCacheEntryOptions? cacheEntryOptions = new MemoryCacheEntryOptions()
                     .SetSlidingExpiration(TimeSpan.FromMinutes(20));
@@ -47,8 +47,6 @@ namespace MikroagressziWiki.Logic.BusinessLogic
                     .Where(q => q.LanguageCode == languageCode).ToList();
 
                 var translationModels = _mapper.MapCollection<Translation, TranslationModel>(translations);
-
-                //Dictionary<string, Dic>
 
                 cacheValue = JsonConvert.SerializeObject(
                     translationModels.ToDictionary(key => key.Key, v => v.Value));
