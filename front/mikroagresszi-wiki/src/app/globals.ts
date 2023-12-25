94c07477096654b1412f8691fe18611f057b9531
Muráncsik Adéna <adena.murancsik@gmail.com>
2023-12-25 15:57:04 +0100

    changing footer

--- front/mikroagresszi-wiki/src/app/globals.ts
+++ front/mikroagresszi-wiki/src/app/globals.ts
@@ -1,4 +1,4 @@
 'use strict';
 
 export const URL_DEV='http://localhost:3200';
-export const URL_LIVE='https://api.mikroagressziok.hu';
+export const URL_LIVE='https://api.mikroagressziok.genderutikalauz.hu';
